ed7e2d1f04baebbc8bcd539e0ce05c07910bb5ec
rockgem <63050183+rockgem@users.noreply.github.com>
2023-02-26 21:03:15 +0800

    rename assets, add items dict

--- scripts/global/ManagerGame.gd
+++ scripts/global/ManagerGame.gd
@@ -4,6 +4,19 @@ extends Node
 signal item_bought
 
 
+var all_items: Dictionary = {}
+
+
+var player_data: Dictionary = {
+	'money': 100
+}
+
+
+
+func _ready():
+	pass
+
+
 func buy_item(item_id: String):
 	
 	
