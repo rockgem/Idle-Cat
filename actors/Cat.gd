f6e76e192cd23f9c7085485ba412b162b5c2c19f
rockgem <63050183+rockgem@users.noreply.github.com>
2023-03-04 12:53:50 +0800

    added book

--- actors/Cat.gd
+++ actors/Cat.gd
@@ -4,10 +4,15 @@ extends Sprite
 
 func _ready():
 	ManagerGame.connect("studying_activated", self, 'on_studying_activated')
+	
+	if ManagerGame.player_data['is_studying']:
+		on_studying_activated(true)
 
 
 func on_studying_activated(b):
 	if b:
 		texture = load("res://assets/furniture_wall_floor_cat/objs/Black cat sitting.png")
+		$Book.show()
 	else:
 		texture = load("res://assets/furniture_wall_floor_cat/objs/Black cat standing.png")
+		$Book.hide()
